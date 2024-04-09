c516e06291672a3336aceb3d7e13a5d1746a7bc4
zukurnaz <77931192+zukurnaz@users.noreply.github.com>
2024-04-09 20:07:17 +0300

    Move Catalog List from admin/ to /admin/catalog, add empty pages for /admin/order and /admin

---
+++ src/Web/Pages/Admin/catalog.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.eShopWeb.ApplicationCore.Constants;
+
+namespace Microsoft.eShopWeb.Web.Pages.Admin
+{
+    [Authorize(Roles = BlazorShared.Authorization.Constants.Roles.ADMINISTRATORS)]
+    public class CatalogModel : PageModel
+    {
+    }
+}
